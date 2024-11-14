44e2a7bf7822e8edcd25902d39174e649db1932e
Ytalo-Alves <ytaloalves10@hotmail.com>
2024-11-13 21:24:06 -0300

    using cookies to validate the transaction user

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -2,64 +2,97 @@ import type { FastifyInstance } from "fastify";
 import { knex } from "../database";
 import { z } from "zod";
 import { randomUUID } from "crypto";
+import checkSessionIdExists from "../middlewares/check-session-id-exists";
 
-export default function TransactionsRoutes(app: FastifyInstance){
-   app.post("/transactions", async (request, reply) => {
 
+
+export default function TransactionsRoutes(app: FastifyInstance) {
+
+  const getTransactionParamsSchema = z.object({
+    id: z.string().uuid(),
+  });
+
+
+
+  app.post("/transactions", async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
       amount: z.number(),
-      type: z.enum(['credit', 'debit'])
-    })
+      type: z.enum(["credit", "debit"]),
+    });
 
-    const { title, amount, type } = createTransactionBodySchema.parse(request.body)
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body
+    );
 
-    let sessionId = request.cookies.sessionId
+    let sessionId = request.cookies.sessionId;
 
-    if (!sessionId){
-      sessionId = randomUUID()
-      reply.cookie('sessionId', sessionId, {
-        path: '/',
+    if (!sessionId) {
+      sessionId = randomUUID();
+      reply.cookie("sessionId", sessionId, {
+        path: "/",
         maxAge: 60 * 60, // 1 hour
-      })
+      });
     }
 
-    await knex('transactions').insert({
+    await knex("transactions").insert({
       id: randomUUID(),
       title,
-      amount: type === 'credit' ? amount : amount * -1,
+      amount: type === "credit" ? amount : amount * -1,
       session_Id: sessionId,
-    })
-  
-    return reply.status(201).send()
+    });
+
+    return reply.status(201).send();
   });
 
-  app.get("/transactions", async (request, reply) => {
-    const transactions = await knex('transactions').select('*')
+  app.get(
+    "/transactions",
+    { preHandler: [checkSessionIdExists] },
+    async (request, reply) => {
+      const { sessionId } = request.cookies;
 
-    return reply.send(transactions)
-  })
+      if (!sessionId) {
+        return reply.status(401).send({ error: "Unauthorized" });
+      }
 
-  const getTransactionParamsSchema = z.object({
-    id: z.string().uuid()
-  })
+      const transactions = await knex("transactions")
+        .where("session_id", sessionId)
+        .select();
 
-  app.get('/transactions/:id', async (request, reply) => {
-    const { id } = getTransactionParamsSchema.parse(request.params)
-    const transaction = await knex('transactions').where('id', id).first()
+      return reply.send(transactions);
+    }
+  );
 
 
+  app.get("/transactions/:id",{preHandler: [checkSessionIdExists]} , async (request) => {
+    const { id } = getTransactionParamsSchema.parse(request.params);
+    const { sessionId } = request.cookies;
 
-    return {transaction}
-  })
+    const transaction = await knex("transactions")
+    .where({
+      session_Id: sessionId,
+      id,
+    })
+    .first();
 
-  app.get('/summary', async () => {
-    const summary = await knex('transactions').sum('amount', {as: 'amount'}).first()
-    const formattedAmount = summary ? new Intl.NumberFormat('pt-BR', {
-      style: 'decimal', 
-      currency:'BRL'
-    }).format(Number(summary.amount)): null;
+    return { transaction };
+  });
 
-    return { summary: {amount: formattedAmount} }
-  })
+  app.get("/summary",{preHandler: [checkSessionIdExists]}, async (request) => {
+
+    const { sessionId } = request.cookies;
+
+    const summary = await knex("transactions")
+      .sum("amount", { as: "amount" })
+      .where('session_id', sessionId)
+      .first();
+    const formattedAmount = summary
+      ? new Intl.NumberFormat("pt-BR", {
+          style: "decimal",
+          currency: "BRL",
+        }).format(Number(summary.amount))
+      : null;
+
+    return { summary: { amount: formattedAmount } };
+  });
 }

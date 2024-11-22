25a0e4f090656448671acadf44f1b02d6ce62647
Ytalo-Alves <ytaloalves@hotmail.com>
2024-11-22 10:48:49 -0300

    Project finesh

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -2,94 +2,66 @@ import type { FastifyInstance } from "fastify";
 import { knex } from "../database";
 import { z } from "zod";
 import { randomUUID } from "crypto";
-import checkSessionIdExists from "../middlewares/check-session-id-exists";
 
+export default function TransactionsRoutes(app: FastifyInstance){
+   app.post("/transactions", async (request, reply) => {
 
-
-export default function TransactionsRoutes(app: FastifyInstance) {
-
-  const getTransactionParamsSchema = z.object({
-    id: z.string().uuid(),
-  });
-
-
-
-  app.post("/transactions", async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
       amount: z.number(),
-      type: z.enum(["credit", "debit"]),
-    });
+      type: z.enum(['credit', 'debit'])
+    })
 
-    const { title, amount, type } = createTransactionBodySchema.parse(
-      request.body
-    );
+    const { title, amount, type } = createTransactionBodySchema.parse(request.body)
 
-    let sessionId = request.cookies.sessionId;
+    let sessionId = request.cookies.sessionId
 
-    if (!sessionId) {
-      sessionId = randomUUID();
-      reply.cookie("sessionId", sessionId, {
-        path: "/",
+    if (!sessionId){
+      sessionId = randomUUID()
+      reply.cookie('sessionId', sessionId, {
+        path: '/',
         maxAge: 60 * 60, // 1 hour
-      });
+      })
     }
 
-    await knex("transactions").insert({
+    await knex('transactions').insert({
       id: randomUUID(),
       title,
-      amount: type === "credit" ? amount : amount * -1,
+      amount: type === 'credit' ? amount : amount * -1,
       session_Id: sessionId,
-    });
-
-    return reply.status(201).send();
-  });
-
-  app.get("/transactions",{ preHandler: [checkSessionIdExists] }, async (request, reply) => {
-      const { sessionId } = request.cookies;
-
-      if (!sessionId) {
-        return reply.status(401).send({ error: "Unauthorized" });
-      }
-
-      const transactions = await knex("transactions")
-        .where("session_id", sessionId)
-        .select();
-
-      return reply.send({transactions});
-    }
-  );
-
-
-  app.get("/transactions/:id",{preHandler: [checkSessionIdExists]} , async (request) => {
-    const { id } = getTransactionParamsSchema.parse(request.params);
-    const { sessionId } = request.cookies;
-
-    const transaction = await knex("transactions")
-    .where({
-      session_Id: sessionId,
-      id,
     })
-    .first();
-
-    return { transaction };
+  
+    return reply.status(201).send()
   });
 
-  app.get("/summary",{preHandler: [checkSessionIdExists]}, async (request) => {
+  app.get("/transactions", async (request, reply) => {
+    const cookie = request.cookies
 
-    const { sessionId } = request.cookies;
+    const transactions = await knex('transactions').select('*')
 
-    const summary = await knex("transactions")
-      .sum("amount", { as: "amount" })
-      .where('session_id', sessionId)
-      .first();
-    const formattedAmount = summary
-      ? new Intl.NumberFormat("pt-BR", {
-          style: "decimal",
-          currency: "BRL",
-        }).format(Number(summary.amount))
-      : null;
+    return reply.send(transactions)
+  })
 
-    return { summary };
-  });
+  const getTransactionParamsSchema = z.object({
+    id: z.string().uuid()
+  })
+ 
+  app.get('/transactions/:id', async (request, reply) => {
+    const { id } = getTransactionParamsSchema.parse(request.params)
+    const transaction = await knex('transactions').where('id', id).first()
+
+
+
+    return {transaction}
+  })
+
+  app.get('/summary', async () => {
+    const summary = await knex('transactions').sum('amount', {as: 'amount'}).first()
+    const formattedAmount = summary ? new Intl.NumberFormat('pt-BR', {
+      style: 'decimal', 
+      currency:'BRL'
+    }).format(Number(summary.amount)): null;
+
+    return { summary: {amount: formattedAmount} }
+  })
 }
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
+import fastify from "fastify";
 import { app } from "./app";
 import { env } from "./env";
 
 const host = process.env.RENDER ? '0.0.0.0' : 'localhost';
+
 app.listen({ port: env.PORT, host }).then(() => {
   console.log(`server is running on port ${env.PORT}`);
 });
